c12b61e086d1440e0f6f337c38f3896946e4e8f6
VerticalHeretic <ls.warss98@gmail.com>
2023-06-08 12:42:48 +0200

    Initial Commit

---
+++ wwdc2023-and-beyond/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wwdc2023-and-beyond
+//
+//  Created by Łukasz Stachnik on 08/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wwdc2023-and-beyond/wwdc2023_and_beyondApp.swift
@@ -0,0 +1,17 @@
+//
+//  wwdc2023_and_beyondApp.swift
+//  wwdc2023-and-beyond
+//
+//  Created by Łukasz Stachnik on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct wwdc2023_and_beyondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
